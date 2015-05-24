6f262754e7a91116401e86816c41e2798e958039
Sven Hertle <sven.hertle@googlemail.com>
2015-05-24 15:05:11 +0200

    generalised settings

--- helfertool/settings.py
+++ helfertool/settings.py
@@ -20,17 +20,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5$pc_$7wwu46qxwf%^0m#2le=!$6g8^i-h9azwr4-y0=fobqf4'
+SECRET_KEY = 'CHANGE-ME-AFTER-INSTALL'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-ADMINS = (('Sven Hertle', 'hertle@fs.tum.de'))
+ADMINS = (('Admin Name', 'admin@localhost'))
 
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
+# for e-mail debugging
+#EMAIL_HOST = 'localhost'
+#EMAIL_PORT = 1025
 
 LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
