4227ccf84e765082c935b23dd7222ecdfdd44a04
Sven Hertle <sven.hertle@googlemail.com>
2015-05-25 14:28:28 +0200

    fixed bug in settings

--- helfertool/settings.py
+++ helfertool/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-ADMINS = (('Admin Name', 'admin@localhost'))
+ADMINS = (('Admin Name', 'admin@localhost'), )
 
 # for e-mail debugging
 #EMAIL_HOST = 'localhost'
