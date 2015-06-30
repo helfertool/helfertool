81b27faca7e2d881792047d172f10198c4e0733a
Sven Hertle <sven.hertle@googlemail.com>
2015-06-30 21:58:28 +0200

    documentation for models.py

--- registration/models.py
+++ registration/models.py
@@ -19,10 +19,18 @@ class Event(models.Model):
     """ Event for registration.
 
     Columns:
+        url_name: the ID of the event used in URLs
         name: the name of the event
         text: text at begin of registration
-        imprint: text at the end of registration
+        imprint: text at the bottom if the registration page
+        registered: text after the successful registration
+        email: e-mail address used as sender of automatic e-mails
         active: is the registration opened?
+        admins: list of admins of this event, they can see and edit everything
+        ask_shirt: ask for the t-shirt size during registration
+        ask_vegetarian: ask, if the helper is vegetarian
+        show_public_numbers: show the number of current and maximal helpers on
+                             the registration page
     """
     url_name = models.CharField(max_length=200, unique=True,
                                 validators=[RegexValidator('^[a-zA-Z0-9]+$')],
@@ -63,6 +71,10 @@ class Event(models.Model):
         return self.name
 
     def is_admin(self, user):
+        """ Check, if a user is admin of this event and returns a boolean.
+
+        A superuser is also admin of an event.
+        """
         return user.is_superuser or self.admins.filter(pk=user.pk).exists()
 
 class Job(models.Model):
@@ -71,6 +83,7 @@ class Job(models.Model):
     Columns:
         event: event of this job
         name: name of the job, e.g, Bierstand
+        infection_instruction: is an instruction for the handling of food necessary?
         description: longer description of the job
     """
     event = models.ForeignKey(Event)
@@ -82,6 +95,11 @@ class Job(models.Model):
         return "%s (%s)" % (self.name, self.event)
 
     def shifts_by_day(self):
+        """ Returns all shifts grouped sorted by day and sorted by time.
+
+        The result is a dict with date objects as keys. Each item of the
+        dictionary is a OrderedDict of shifts.
+        """
         tmp_shifts = dict()
 
         # iterate over all shifts and group them by day
@@ -126,21 +144,35 @@ class Shift(models.Model):
         return "%s %s (%s)" % (self.job.name, date_f(localtime(self.begin), 'DATETIME_FORMAT'), self.job.event)
 
     def time(self):
+        """ Returns a string representation of the begin and end time.
+
+        The begin contains the date and time, the end only the time.
+        """
         return "%s, %s - %s" % (date_f(localtime(self.begin), 'DATE_FORMAT'),
                                 date_f(localtime(self.begin), 'TIME_FORMAT'),
                                 date_f(localtime(self.end), 'TIME_FORMAT'))
 
     def time_hours(self):
+        """ Returns a string representation of the begin and end time.
+
+        Only the time is used, the date is not shown.
+        """
         return "%s - %s" % (date_f(localtime(self.begin), 'TIME_FORMAT'),
                             date_f(localtime(self.end), 'TIME_FORMAT'))
 
     def num_helpers(self):
+        """ Returns the current number of helpers- """
         return self.helper_set.count()
 
     def is_full(self):
+        """ Check if the shift is full and return a boolean. """
         return self.num_helpers() >= self.number
 
     def helpers_percent(self):
+        """ Calculate the percentage of registered helpers and returns an int.
+
+        If the maximal number of helpers for a shift is 0, 0 is returned.
+        """
         if self.number == 0:
             return 0
 
@@ -148,6 +180,11 @@ class Shift(models.Model):
 
 @receiver(pre_delete, sender=Shift, dispatch_uid='shift_delete')
 def shift_delete(sender, instance, using, **kwargs):
+    """ Delete helpers without shifts, when a shift is deleted.
+
+    This is a signal handler, that is called, when a shift is deleted. It
+    deletes all helpers, that are only registered for this single shift.
+    """
     # delete helpers, that have only one shift, when this shift is deleted
     for helper in instance.helper_set.all():
         if helper.shifts.count() == 1:
@@ -158,13 +195,17 @@ class Helper(models.Model):
 
     Columns:
         shifts: all shifts of this person
-        prename
-        surname
-        email
-        phone
-        comment
-        shirt (possible sizes are defined here)
+        prename: the prename
+        surname: the surname
+        email: the e-mail address
+        phone: phone number
+        comment: optional comment
+        shirt: t-shirt size (possible sizes are defined here)
+        vegetarian: is the helper vegetarian?
+        infection_instruction: status of the instruction for food handling
+        timestamp: time of registration
     """
+
     SHIRT_S = 'S'
     SHIRT_M = 'M'
     SHIRT_L = 'L'
@@ -236,12 +277,17 @@ class Helper(models.Model):
         return "%s %s" % (self.prename, self.surname)
 
     def get_infection_instruction_short(self):
+        """ Returns the short description for the infection_instruction field. """
         for item in Helper.INSTRUCTION_CHOICES_SHORT:
             if item[0] == self.infection_instruction:
                 return item[1]
         return ""
 
     def send_mail(self):
+        """ Send a confirmation e-mail to the registered helper.
+
+        This e-mail contains a list of the shifts, the helper registered for.
+        """
         if self.shifts.count() == 0:
             return
 
@@ -258,6 +304,12 @@ class Helper(models.Model):
 
     @property
     def event(self):
+        """ Returns the event, where this person helps us.
+
+        If the helper is not registered for any shift, None is returned (this
+        should not happen).
+        """
+
         if self.shifts.count() == 0:
             return None
 
