6ac16601a3d4e150562d72d2fda052cf4f811cef
Sven Hertle <sven.hertle@googlemail.com>
2016-03-05 15:12:45 +0100

    added script to detect helpers that have to be deleted

---
+++ registration/management/commands/checkdelbug.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+from registration.models import Helper
+
+
+class Command(BaseCommand):
+    help = 'Checks for helpers that have no shift and are not coordinators'
+
+    def handle(self, *args, **options):
+        for helper in Helper.objects.all():
+            if helper.shifts.count() == 0 and not helper.is_coordinator:
+                self.stdout.write('{}\t\t{}'.format(str(helper), helper.id))
