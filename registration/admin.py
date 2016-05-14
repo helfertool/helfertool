c88c3854c13746f3d100f715f5198adad1650ddf
Sven Hertle <sven.hertle@googlemail.com>
2016-05-14 15:33:19 +0200

    django admin: make helpers searchable

--- registration/admin.py
+++ registration/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 from .models import Event, Job, Shift, Helper
 
+class HelperAdmin(admin.ModelAdmin):
+    search_fields = ['firstname', 'surname', 'email', ]
+
+
 admin.site.register(Event)
 admin.site.register(Job)
 admin.site.register(Shift)
-admin.site.register(Helper)
+admin.site.register(Helper, HelperAdmin)
