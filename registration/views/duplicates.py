04277cda6db9c77685f27d002ee2ab228b7503e7
Sven Hertle <sven.hertle@googlemail.com>
2016-05-24 00:45:50 +0200

    fixed bug in duplicate detection

--- registration/views/duplicates.py
+++ registration/views/duplicates.py
@@ -2,9 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 
+from collections import OrderedDict
+
 from .utils import nopermission
 
-from ..models import Event, Helper
+from ..models import Event
 
 
 @login_required
@@ -15,14 +17,14 @@ def duplicates(request, event_url_name):
     if not event.is_admin(request.user):
         return nopermission(request)
 
-    duplicates = Helper.objects.values('email', 'firstname', 'surname').annotate(
-        email_count=Count('email')).exclude(email_count=1)
+    duplicates = event.helper_set.values('email').annotate(
+        email_count=Count('email')).exclude(email_count=1).order_by('email')
 
-    duplicated_helpers = {}
+    duplicated_helpers = OrderedDict()
 
     for dup in duplicates:
-        name = "{} {}".format(dup['firstname'], dup['surname'])
-        duplicated_helpers[name] = Helper.objects.filter(email=dup['email'])
+        duplicated_helpers[dup['email']] = event.helper_set.filter(
+            email=dup['email'])
 
     # overview over jobs
     context = {'event': event,
