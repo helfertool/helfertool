3ad77a4ae1ebe446c6712ba3060cd1b50fc4f56f
Sven Hertle <sven.hertle@googlemail.com>
2017-03-26 00:36:54 +0100

    moved news before registration in urls

--- helfertool/urls.py
+++ helfertool/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
 
     url(r'', include('help.urls')),
+    url(r'', include('news.urls')),
     url(r'', include('registration.urls')),
     url(r'', include('statistic.urls')),
     url(r'', include('badges.urls')),
     url(r'', include('gifts.urls')),
     url(r'', include('inventory.urls')),
     url(r'', include('mail.urls')),
-    url(r'', include('news.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
