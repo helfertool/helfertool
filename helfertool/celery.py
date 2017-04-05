773d5c8c10614ff1fc17fff00a9691a49d6d2331
Sven Hertle <sven.hertle@googlemail.com>
2017-04-05 19:00:56 +0200

    updated celery settings from documentation

--- helfertool/celery.py
+++ helfertool/celery.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 
 import os
 
@@ -7,14 +7,12 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helfertool.settings")
 
-from django.conf import settings  # noqa
-
 app = Celery('helfertool')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
 
 
 @app.task(bind=True)
