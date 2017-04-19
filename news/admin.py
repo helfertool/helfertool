2931d4cafe8446f3e8f9acb30c48aa1b59d6792b
Sven Hertle <sven.hertle@googlemail.com>
2017-04-19 11:48:56 +0200

    news: search for mail address in admin interface

--- news/admin.py
+++ news/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Person
 
-admin.site.register(Person)
+
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = [ 'email', ]
+
+admin.site.register(Person, PersonAdmin)
