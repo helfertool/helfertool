825e6793bade283210712399463e314d948103af
Sven Hertle <sven.hertle@googlemail.com>
2017-04-26 14:10:51 +0200

    admin fields: show also username

--- registration/forms/fields/user.py
+++ registration/forms/fields/user.py
@@ -4,5 +4,5 @@ from .duallist import DualListField
 class UserSelectField(DualListField):
     def label_from_instance(self, obj):
         if obj.first_name and obj.last_name:
-            return obj.get_full_name()
+            return "{} ({})".format(obj.get_full_name(), obj.get_username())
         return obj.get_username()
