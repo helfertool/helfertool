4e8c0f1c50fc3bbe1fd3b0b1a8caf349a3a2e09b
Sven Hertle <sven.hertle@googlemail.com>
2018-11-01 19:42:58 +0100

    use different tags for docker images

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,12 +6,21 @@ set -e
 basedir="$(dirname "$(dirname "$(readlink -f "$0")")")"
 cd "$basedir"
 
+# get git branch and determine tag
+git_branch="$(git rev-parse --abbrev-ref HEAD)"
+
+if [ "$git_branch" == "master" ] ; then
+    docker_tag="helfertool/helfertool:latest"
+else
+    docker_tag="helfertool/helfertool:$git_branch"
+fi
+
 # build
 if [ "$1" == "build" ] ; then
-    docker build --no-cache --pull -t helfertool/helfertool .
-# build
+    docker build --no-cache --pull -t "$docker_tag" .
+# fastbuild
 elif [ "$1" == "fastbuild" ] ; then
-    docker build -t helfertool/helfertool .
+    docker build -t "$docker_tag" .
 # test
 elif [ "$1" == "test" ] ; then
     docker run --rm \
@@ -20,11 +29,24 @@ elif [ "$1" == "test" ] ; then
         -v "$PWD/test/docker/data:/data" \
         -v "$PWD/test/docker/config:/config" \
         -v "$PWD/test/docker/log:/log" \
-        helfertool/helfertool
+        "$docker_tag"
 # push
 elif [ "$1" == "push" ] ; then
-    docker push helfertool/helfertool
+    docker push "$docker_tag"
+# pushlatest
+elif [ "$1" == "pushlatest" ] ; then
+    docker tag "$docker_tag" "helfertool/helfertool:latest"
+    docker push "helfertool/helfertool:latest"
 else
-    echo "Commands: build, fastbuild, test, push"
+    echo -e "Usage:"
+    echo
+    echo -e "build\t\tBuild clean image"
+    echo -e "fastbuild\tBuild image using cached layers"
+    echo
+    echo -e "test\t\tRun docker container with test data"
+    echo
+    echo -e "push\t\tPush container for current branch to docker hub"
+    echo -e "publishdev\tPush current container as \"latest\" to docker hub"
+
     exit 1
 fi
