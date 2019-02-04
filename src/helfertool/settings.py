d7a52febae3d1d2eab81598a9da14818ba504d3f
Sven Hertle <hertle@narfi.net>
2019-02-04 19:18:02 +0100

    removed XFrameOptionsMiddleware since it is set by the web server

--- src/helfertool/settings.py
+++ src/helfertool/settings.py
@@ -365,7 +365,6 @@ MIDDLEWARE = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'helfertool.urls'
