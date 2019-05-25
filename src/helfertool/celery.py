c62b88dc02a106f1cd8bebe16b5e62ff60888fee
Sven Hertle <hertle@narfi.net>
2019-05-25 19:56:12 +0200

    merged celery things in helfertool app in one file

--- src/helfertool/celery.py
+++ src/helfertool/celery.py
@@ -2,7 +2,12 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 
+from django.conf import settings
+from django.core.mail import mail_admins
+from django.views.debug import ExceptionReporter
+
 from celery import Celery
+from celery.signals import task_failure
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helfertool.settings")
@@ -18,3 +23,17 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+@task_failure.connect
+def celery_error_handler(task_id, exception, traceback, einfo, *args, **kwargs):
+    if settings.DEBUG:
+        return
+
+    mail_subject = "Task exception - {}".format(exception)
+    mail_subject = mail_subject.replace("\n", " ")[:250]
+
+    reporter = ExceptionReporter(None, einfo.type, exception, traceback)
+    mail_text = reporter.get_traceback_text()
+
+    mail_admins(mail_subject, mail_text)
