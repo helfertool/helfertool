74644227ece47f3240fee0feb4a5950243cb9b9d
Sven Hertle <hertle@narfi.net>
2020-04-10 16:04:48 +0200

    use some logic for shift overlappings in front and backend

--- src/helfertool/static/helfertool/js/registration.js
+++ src/helfertool/static/helfertool/js/registration.js
@@ -1,28 +1,42 @@
-allowed_overlap = parseInt($('#max_overlapping').val()) * 60;
+max_overlapping = parseInt($('#register_form').data('max-overlapping'));
 
-function update_shit_registration(element) {
+function update_shift_registration(input_field) {
+    // do not check, if the overlapping setting is None (in python)
+    if(isNaN(max_overlapping)) {
+        return
+    }
+
     var modifier = null;
-    if (element.checked) {
-        // the user has activated a shift, disable all colliding shifts
+    if (input_field.checked) {
+        // the user has selected a shift, disable all colliding shifts
         modifier = function(e){
+            // when enabling a shift lateron again, we need to know why the shift was disabled
+            // if there were other additional colliding shift, we cannot enable the shift.
+            // so we store the colliding shifts for every single shift here
             if (!$(e).data('colliders')) {
                 $(e).data('colliders', {})
             }
-            $(e).data('colliders')[element.id] = element;
+            $(e).data('colliders')[input_field.id] = input_field;
+
+            // now disable the input field
             e.disabled = true;
             $(e.parentNode).addClass('colliding');
             $(e.parentNode).removeClass('notcolliding');
         }
     } else {
+        // the user has removed a shift selection, enable the colliding shifts again
         modifier = function(e){
+            // we can only enable the shift, if we have no other colliding shifts
             if (!$(e).data('colliders')) {
                 $(e).data('colliders', {})
             }
 
-            if ($(e).data('colliders')[element.id]) {
-                delete $(e).data('colliders')[element.id];
+            // so first remove this one
+            if ($(e).data('colliders')[input_field.id]) {
+                delete $(e).data('colliders')[input_field.id];
             }
 
+            // and the check if we have other ones
             if ($.isEmptyObject($(e).data('colliders'))) {
                 e.disabled = false;
                 $(e.parentNode).removeClass('colliding');
@@ -31,31 +45,42 @@ function update_shit_registration(element) {
         }
     }
 
-    var newshift = element
-    var start = $(element).data('start');
-    var end = $(element).data('end');
-    $('.shift_registration').each(function(i, element) {
-        if (newshift == element) return;
-        var mystart = $(element).data('start') + allowed_overlap;
-        var myend = $(element).data('end') - allowed_overlap;
+    // check for overlapping shifts
+    var begin = $(input_field).data('begin');
+    var end = $(input_field).data('end');
+    var max_overlapping_seconds = max_overlapping * 60;
 
-        if ((mystart > start && mystart < end) ||
-            (myend > start   && myend < end) ||
-            (start > mystart && start < myend) ||
-            (end > mystart   && end < myend)) {
-            modifier(element);
+    $('input.registration_possible').each(function(i, other_field) {
+        // skip the handled shift itself
+        if (input_field == other_field) {
+            return
+        }
+
+        var other_begin = $(other_field).data('begin');
+        var other_end = $(other_field).data('end');
+
+        // this is the same logic as in RegisterForm, so the frontend and backend reject the same shifts
+        is_overlapping = (other_end-begin > max_overlapping_seconds &&
+                          end-other_begin > max_overlapping_seconds) ||
+                         (begin >= other_begin && end <= other_end) ||
+                         (other_begin >= begin && other_end <= end);
+
+        if (is_overlapping) {
+            modifier(other_field);
         }
     });
 
 }
 
-$('.shift_registration').each(function (i, element) {
-    update_shit_registration(element);
+// update and register event handler for every field
+$('input.registration_possible').each(function (i, element) {
+    update_shift_registration(element);
     $(this).change(function () {
-        update_shit_registration(this);
+        update_shift_registration(this);
     });
 });
 
+/* Infection instruction field */
 function handle_infection_instruction()
 {
     var show_field = 0;
