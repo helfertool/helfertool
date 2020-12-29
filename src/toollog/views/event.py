af78ccdddce708c82d0f66e3409a70c66466468a
Sven Hertle <hertle@narfi.net>
2020-12-29 22:32:51 +0100

    event audit log is cleared when event is archived
    the "event archived" log entry is still there

--- src/toollog/views/event.py
+++ src/toollog/views/event.py
@@ -2,7 +2,6 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
-from registration.decorators import archived_not_available
 from registration.models import Event
 from registration.permissions import has_access, ACCESS_EVENT_VIEW_AUDITLOGS
 from registration.views.utils import nopermission
@@ -11,7 +10,6 @@ from ..models import LogEntry
 
 
 @login_required
-@archived_not_available
 def event_audit_log(request, event_url_name):
     event = get_object_or_404(Event, url_name=event_url_name)
 
