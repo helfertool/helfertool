0684de374e971d35d38e917f17446da223bfa51b
Sven Hertle <hertle@narfi.net>
2021-01-09 20:06:07 +0100

    fixed celery tasks

--- src/mail/tasks.py
+++ src/mail/tasks.py
@@ -1,13 +1,13 @@
 from __future__ import absolute_import
 
-from celery import shared_task
+from celery import task
 from django.conf import settings
 
 from helfertool.utils import cache_lock
 from .receive import MailHandler
 
 
-@shared_task(bind=True)
+@task(bind=True)
 def receive_mails(self):
     if settings.RECEIVE_EMAIL_HOST:
         with cache_lock("receive_mails", self.app.oid) as acquired:
--- src/registration/apps.py
+++ src/registration/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class RegistrationConfig(AppConfig):
     name = 'registration'
-
-#    def ready(self):
-#        from . import tasks
-#        tasks.setup_event_flags.delay()
