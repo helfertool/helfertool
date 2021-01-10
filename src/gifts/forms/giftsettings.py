31b4b9d843616f6074479123565895af7a6b3ea5
Sven Hertle <hertle@narfi.net>
2021-01-10 15:08:33 +0100

    disable gift settings form is event is archived

--- src/gifts/forms/giftsettings.py
+++ src/gifts/forms/giftsettings.py
@@ -7,3 +7,10 @@ class GiftSettingsForm(forms.ModelForm):
     class Meta:
         model = GiftSettings
         exclude = ['event', ]
+
+    def __init__(self, *args, **kwargs):
+        super(GiftSettingsForm, self).__init__(*args, **kwargs)
+
+        if self.instance.event.archived:
+            for field_id in self.fields:
+                self.fields[field_id].disabled = True
