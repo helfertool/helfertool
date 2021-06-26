6764c166d57c4d4d61f8e75c725396990cdd1f5e
Sven Hertle <hertle@narfi.net>
2021-06-26 19:05:28 +0200

    minor design fixes

--- src/helfertool/static/helfertool/theme/helfertool.js
+++ src/helfertool/static/helfertool/theme/helfertool.js
@@ -1,5 +1,5 @@
-// Toggle sidemenu
 $(document).ready(function () {
+    // Register event handler to toggle sidemenu
     $('#toggle_sidemenu').on('click', function () {
         $('#sidemenu').toggleClass('visibility');
     });
