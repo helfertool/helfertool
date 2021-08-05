85662abf3cb444eeb314dca4525d95723698d690
Sven Hertle <hertle@narfi.net>
2021-08-05 18:03:17 +0200

    add setting for custom text during newsletter subscription
    fixes #123

--- src/toolsettings/views/__init__.py
+++ src/toolsettings/views/__init__.py
@@ -1,2 +1,3 @@
-from .templates import templates, template_about, template_privacy, template_login, template_add_user
+from .templates import templates, template_about, template_privacy, template_login, template_add_user, \
+    template_newsletter
 from .check import check
