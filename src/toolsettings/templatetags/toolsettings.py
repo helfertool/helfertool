77b37d9248a1a7079f0733327167b517b5fa79d9
Sven Hertle <hertle@narfi.net>
2021-08-08 20:46:49 +0200

    merge templatetag library setting into toolsettings

--- src/toolsettings/templatetags/toolsettings.py
+++ src/toolsettings/templatetags/toolsettings.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 from django.utils.safestring import mark_safe
 
 from ..models import HTMLSetting, TextSetting
@@ -7,11 +8,19 @@ from ..utils import get_setting
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True)
-def htmlsetting(context, key):
+@register.simple_tag
+def htmlsetting(key):
+    """ Return HTML setting from database. """
     return mark_safe(get_setting(HTMLSetting, key, ""))
 
 
-@register.simple_tag(takes_context=True)
-def textsetting(context, key):
+@register.simple_tag
+def textsetting(key):
+    """ Return text setting from database. """
     return get_setting(TextSetting, key, "")
+
+
+@register.simple_tag
+def djangosetting(name):
+    """ Return setting from Django configuration. """
+    return getattr(settings, name, "")
