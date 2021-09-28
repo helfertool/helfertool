7397b35d6418f257aa86273088ded08f67b53c85
Sven Hertle <hertle@narfi.net>
2021-09-28 21:14:21 +0200

    view for editing corona tracing data

--- src/corona/urls.py
+++ src/corona/urls.py
@@ -7,4 +7,12 @@ urlpatterns = [
     url(r'^(?P<event_url_name>[a-zA-Z0-9]+)/corona/settings/$',
         views.settings,
         name='settings'),
+
+    url(r'^(?P<event_url_name>[a-zA-Z0-9]+)/helpers/(?P<helper_pk>[0-9a-f\-]+)/corona/$',
+        views.view_helper,
+        name='view_helper'),
+
+    url(r'^(?P<event_url_name>[a-zA-Z0-9]+)/helpers/(?P<helper_pk>[0-9a-f\-]+)/corona/edit/$',
+        views.edit_helper,
+        name='edit_helper'),
 ]
--- src/corona/views/__init__.py
+++ src/corona/views/__init__.py
@@ -1,1 +1,2 @@
+from .data import view_helper, edit_helper
 from .settings import settings
