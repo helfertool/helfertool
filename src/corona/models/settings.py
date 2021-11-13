2639d1fa65c239938bdf603104cac28a650fd331
Sven Hertle <hertle@narfi.net>
2021-11-13 19:46:00 +0100

    covid-19: add 2g plus

--- src/corona/models/settings.py
+++ src/corona/models/settings.py
@@ -6,11 +6,13 @@ from copy import deepcopy
 
 class CoronaSettings(models.Model):
     RULES_2G = '2G'
+    RULES_2Gplus = '2Gplus'
     RULES_3G = '3G'
     RULES_3Gplus = '3Gplus'
 
     RULES_CHOICES = (
         (RULES_2G, _("2G")),
+        (RULES_2Gplus, _("2G plus")),
         (RULES_3G, _("3G")),
         (RULES_3Gplus, _("3G plus")),
     )
